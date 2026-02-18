77347bc316f7494d65c4bb19ca39a71791027575
Olek Zabłocki <olekz@google.com>
2026-02-18 11:19:23 +0000

    Add comments about what the script does

--- hack/bug-repro/test.sh
+++ hack/bug-repro/test.sh
@@ -4,15 +4,24 @@ source common-macros.sh
 
 CLUSTERS="manager worker1 worker2"
 
+# Clean any earlier Kueue objects:
 mkclear
+
+# Create a Kueue object setup:
+# - on manager: a CQ fitting 4 instances of "job1"
+# - on both workers: a CQ fitting 2 instances of "job1"
 mkapply setup1.yaml
 
 kcfg -c kind-manager -g MultiKueueRedoAdmissionOnEvictionInWorker=true
 
 logs_start
 
+# Sumit 6 instances of "job1"
+# (2 will be scheduled on each worker; 2 will remain unadmitted)
 ksend -c kind-manager -n 6 job1.yaml
 sleep 5
+
+# Submit 1 instance of "job1" but with increased priority
 ksend -c kind-manager -p 10 job1.yaml
 sleep 10
 
